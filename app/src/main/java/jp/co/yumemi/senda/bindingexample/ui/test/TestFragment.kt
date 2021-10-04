8fab1feb2115b9c2ed946cc559ddf583bca39098
Seo-4d696b75 <s.kaoru509@gmail.com>
2021-10-04 18:59:28 +0900

    修正：テストフラグメント
    複数の方法で binding を利用して UI へデータを反映する

--- app/src/main/java/jp/co/yumemi/senda/bindingexample/ui/test/TestFragment.kt
+++ app/src/main/java/jp/co/yumemi/senda/bindingexample/ui/test/TestFragment.kt
@@ -13,25 +13,34 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import jp.co.yumemi.senda.bindingexample.R
 import jp.co.yumemi.senda.bindingexample.databinding.FragmentTestBinding
+import jp.co.yumemi.senda.bindingexample.ui.useViewBinding
+import jp.co.yumemi.senda.bindingexample.ui.viewBinding
 
 class TestFragment : Fragment(R.layout.fragment_test) {
 
     private val testViewModel by activityViewModels<TestViewModel>()
 
-    private var binding: FragmentTestBinding? = null
+    private var binding1: FragmentTestBinding? = null
+    private val binding2 by viewBinding<FragmentTestBinding>()
+    private val binding3 by useViewBinding<FragmentTestBinding>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.d("TestFragment", "onViewCreated")
-        binding = FragmentTestBinding.bind(view).also {
+        binding1 = FragmentTestBinding.bind(view).also {
             it.viewModel = testViewModel
-            it.lifecycleOwner = this
+            it.lifecycleOwner = viewLifecycleOwner
+        }
+        testViewModel.text.observe(viewLifecycleOwner){
+            binding1?.textBinding1?.text = it
+            binding2.textBinding2.text = it
+            binding3.textBinding3.text = it
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         Log.d("TestFragment", "onDestroyView")
-        binding = null
+        binding1 = null
     }
 }
